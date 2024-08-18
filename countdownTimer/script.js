bde84a7ccbc2dd1544f45730581b935fcb12ba6c
muirurimukundi <themasalacollective@gmail.com>
2024-08-18 05:55:50 -0400

    Logic to calculate remainder days, hours, minutes, and seconds

--- countdownTimer/script.js
+++ countdownTimer/script.js
@@ -4,12 +4,17 @@ function countDown() {
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
 
-  const seconds = new Date(newYearsDate - currentDate) / 1000;
+  const totalSeconds = new Date(newYearsDate - currentDate) / 1000;
 
-  const days = Math.floor(seconds / 3600 / 24); 
+  const days = Math.floor( seconds / 3600 / 24); 
+  const hours = Math.floor( seconds / 3600 ) % 24;
+  const minutes = Math.floor( seconds/60 )  % 60;
+  const seconds = Math.floor( totalSeconds )  % 60
   
+  console.log(days, hours, minutes, seconds); 
 
 }
 
 //  initial call 
-setInterval(countDown, 1000);
+countDown();
+setInterval( countdown, 1000 )
