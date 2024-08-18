7ea0b7e97be731fbe0dd98f7b9efd54326bdf360
muirurimukundi <themasalacollective@gmail.com>
2024-08-18 05:47:55 -0400

    Calculate the seconds

--- countdownTimer/script.js
+++ countdownTimer/script.js
@@ -1,11 +1,15 @@
-const newYears = "1st Jan 2024";
+const newYears = "1st Jan 2025";
 
 function countDown() {
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
 
-  console.log(newYearsDate - currentDate);
+  const seconds = new Date(newYearsDate - currentDate) / 1000;
+
+  const days = Math.floor(seconds / 3600 / 24); 
+  
+
 }
 
 //  initial call 
-setInterval(countdown, 1000);
+setInterval(countDown, 1000);
