effabe407d262378e0c59a5755db7fd82b7b082d
muirurimukundi <themasalacollective@gmail.com>
2024-08-28 17:15:42 -0400

    Add: array of object representing the data for the app

---
+++ quizApp/script.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+  question: 'How old is Florin?',
+  a: '10',
+  b: '17',
+  c: '26',
+  d: '110',
+  correct: 'c'
+  }, 
+  {
+  question: 'What is the most popular programming language in 2024?',
+    a: 'javaScript',
+    b: 'python',
+    c: 'C',
+    d: 'Ruby',
+    correct: 'a'
+  }, 
+  {
+  question: 'Who is the President of US?',
+    a: 'Taylor Swift',
+    b: 'Donald Trump',
+    c: 'Ivan Saldano',
+    d: 'Joe Biden',
+    correct: 'd'
+  }, 
+  {
+  question: 'What does HTML stand for?',
+    a: 'Hypertext Markup Language',
+    b: 'How To Make Love',
+    c: 'Cascading Style Sheet',
+    d: 'Jason Object Notation',
+    correct: 'a'
+  },
+  {
+    question: 'What year was Javascript lanched?',
+    a: '1996',
+    b: '1995',
+    c: '1994',
+    d: '1993',
+    correct: 'b'
+  }
+]
